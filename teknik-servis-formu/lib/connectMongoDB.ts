951109c980a6cc45c1538daf0b165324e35fd00a
aytunc <aytunc04@hotmail.com>
2025-01-29 12:28:29 +0300

    last commit

--- teknik-servis-formu/lib/connectMongoDB.ts
+++ teknik-servis-formu/lib/connectMongoDB.ts
@@ -15,34 +15,14 @@ if (!MONGODB_DB) {
 	);
 }
 
-interface CachedConnection {
-	client: MongoClient | null;
-	db: Db | null;
-}
-
-declare global {
-	// Ensuring global cache to prevent multiple DB connections in Next.js hot-reloading
-	var mongo: CachedConnection;
-}
-
-const cached: CachedConnection = global.mongo || { client: null, db: null };
-
 export async function connectToDatabase(): Promise<{
 	client: MongoClient;
 	db: Db;
 }> {
-	if (cached.client && cached.db) {
-		return { client: cached.client, db: cached.db };
-	}
-
 	const client = new MongoClient(MONGODB_URI);
 
 	await client.connect();
 	const db = client.db(MONGODB_DB);
-
-	cached.client = client;
-	cached.db = db;
-	global.mongo = cached;
 
 	return { client, db };
 }
